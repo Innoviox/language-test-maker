1d7171ffe001f4375c9c6ce182c99988ed34d1c7
Simon Chervenak <sarras305@gmail.com>
2019-10-16 12:39:34 -0400

    proof-of-concept

--- gui.py
+++ gui.py
@@ -11,6 +11,11 @@ class MainWindow(QDialog):
         self.form = Ui_Dialog()
         self.form.setupUi(self)
 
+        self._initialize()
+        
+        log.info("Set up UI")
+
+    def _initialize(self):
         self.translations = []
         
         for i, word in enumerate(gen_words(n=5), start=1):
@@ -20,38 +25,63 @@ class MainWindow(QDialog):
             #     w, tw, lang = tw, w, dest_lang
             self.translations.append(tw)
 
-            getattr(self.form, f"textBrowser_{i}").setText(w)
+            tb = getattr(self.form, f"textBrowser_{i}")
+            tb.setText(w)
+            tb.repaint()
+            
             getattr(self.form, f"commandLinkButton_{i}").clicked.connect(partial(sentence_to_audio, w, lang, i))
 
-        self.form.check.clicked.connect(self.accept)
+            line = self.get_line(i - 1)
+            line.setText("")
+            self.set_color(line, "#FFFFFF")
+
+        self.form.check.setText("Check")
+        self.form.check.repaint()
+        
+        self._reconn(self.check)
+        
         self.form.show.clicked.connect(self.show_)
 
-        log.info("Set up UI")
-
-    def accept(self):
+    def check(self):
+        log.debug("Received check")
         for i in range(5):
-            line = getattr(self.form, f"lineEdit_{i + 1}")
+            line = self.get_line(i)
             ans, corr = line.text(), self.translations[i]
-            
-            if ans == corr:
-                line.setStyleSheet("background-color: #00FF00")
-            else:
-                line.setStyleSheet("background-color: #FF0000")
 
-            line.style().unpolish(line)
-            line.style().polish(line)
-            line.update()
+            self.set_color(line, "#00FF00" if ans == corr else "#FF0000")
+
+        self.set_next()
 
     def show_(self):
+        log.debug("Received show")
         for i in range(5):
-            line = getattr(self.form, f"lineEdit_{i + 1}")
+            line = self.get_line(i)
 
             line.setText(self.translations[i])
-            
-            line.setStyleSheet("background-color: #FFFFFF")
-            line.style().unpolish(line)
-            line.style().polish(line)
-            line.update()
+            self.set_color(line, "#FFFFFF")
+
+        self.set_next()
+
+    def set_next(self):
+        if all(self.get_line(i).text() == self.translations[i] for i in range(5)):
+            self.form.check.setText("Next")
+            self.form.check.repaint()
+            self._reconn(self._initialize)
+            log.info("All correct!")
+
+    def get_line(self, i):
+        return getattr(self.form, f"lineEdit_{i + 1}")
+    
+    def set_color(self, line, color):
+        line.setStyleSheet(f"background-color: {color}")
+        line.repaint()
+
+    def _reconn(self, new):
+        try:
+            self.form.check.clicked.disconnect()
+        except:
+            pass
+        self.form.check.clicked.connect(new)
 
         
 
