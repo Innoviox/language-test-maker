ab34f6a222efa4d288f98b376beeb70f51905926
Simon Chervenak <sarras305@gmail.com>
2019-10-17 18:39:26 -0400

    fix error

--- ml/testgan.py
+++ ml/testgan.py
@@ -109,7 +109,7 @@ def gen_imgs(model, epoch, test):
     for i in range(predictions.shape[0]):
         plt.subplot(4, 4, i+1)
         plt.imshow(predictions[i, :, :, 0] * 127.5 + 127.5, cmap='gray')
-        plt.axiss('off')
+        plt.axis('off')
 
     plt.savefig('image_at_epoch_{:04d}.png'.format(epoch))
     plt.show()
