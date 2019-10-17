ccefb25168e6f280acdfccc494fe3c625fe9a64c
Simon Chervenak <sarras305@gmail.com>
2019-10-17 18:20:31 -0400

    testgan time

--- ml/test.py
+++ ml/test.py
@@ -4,31 +4,67 @@ warnings.simplefilter(action='ignore', category=FutureWarning)
 import tensorflow as tf
 
 from tensorflow.keras import datasets, layers, models
-
-(train_imgs, train_labs), (test_imgs, test_labs) = datasets.cifar10.load_data()
-train_imgs, test_imgs = train_imgs / 255, test_imgs / 255 # normalize
+from tqdm import tqdm
 
 class_names = ['airplane', 'automobile', 'bird', 'cat', 'deer',
                'dog', 'frog', 'horse', 'ship', 'truck']
 
+(train_imgs, train_labs), (test_imgs, test_labs) = datasets.cifar10.load_data()
+train_imgs, test_imgs = train_imgs / 255, test_imgs / 255 # normalize
+
+##train_labs = []
+##for i in tqdm(_train_labs):
+##    x = [0] * len(class_names)
+##    x[i[0]] = 1
+##    train_labs.append(x)
+##
+##test_labs = []
+##for i in tqdm(_test_labs):
+##    x = [0] * len(class_names)
+##    x[i[0]] = 1
+##    test_labs.append(x)
+
 # CNN: input tensor is (image height, image width, color channels)
 
+##model = models.Sequential()
+##model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)))
+##model.add(layers.MaxPooling2D((2, 2)))
+##model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+##model.add(layers.MaxPooling2D((2, 2)))
+##model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+##
+##model.add(layers.Flatten())
+##model.add(layers.Dense(64, activation='relu'))
+##model.add(layers.Dense(len(class_names), activation='softmax'))
+
 model = models.Sequential()
-model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)))
-model.add(layers.MaxPooling2D((2, 2)))
-model.add(layers.Conv2D(64, (3, 3), activation='relu'))
-model.add(layers.MaxPooling2D((2, 2)))
-model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+# model.add(layers.Input((1,)))
+# model.add(layers.Dense(len(class_names), activation='softmax'))
+model.add(layers.Dense(64*3*64, activation='relu', input_shape=(1,)))
+model.add(layers.BatchNormalization())
+model.add(layers.LeakyReLU())
+# model.add(layers.Flatten())
+model.add(layers.Reshape((64, 3, 64)))
 
-model.add(layers.Flatten())
-model.add(layers.Dense(64, activation='relu'))
-model.add(layers.Dense(len(class_names), activation='softmax'))
+model.add(layers.Conv2DTranspose(64, (3, 3), strides=(1, 1), padding='same', use_bias=False))
+# model.add(layers.BatchNormalization())
+# model.add(layers.LeakyReLU())
 
-# model.summary()
+# model.add(layers.Conv2DTranspose(32, (3, 3), strides=(1, 1), padding='same', use_bias=False))
+
+# model.add(layers.Permute((64, 3, 3), input_shape=(1024,)))
+# model.add(layers.Dense(64, (3, 3), activation='relu'))
+# model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+# model.add(layers.MaxPooling2D((2, 2)))
+# model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+# model.add(layers.MaxPooling2D((2, 2)))
+# model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)))
+
+model.summary()
 
 model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
-history = model.fit(train_imgs, train_labs, epochs=10, validation_data=(test_imgs, test_labs))
+history = model.fit(train_labs, train_imgs, epochs=10, validation_data=(test_labs, test_imgs))
 
 # test_loss, test_acc = model.evaluate(test_imgs, test_labs, verbose=2)
 # print(test_acc)
---
+++ ml/testgan.py
@@ -0,0 +1,148 @@
+import tensorflow as tf
+
+import glob, imageio, numpy as np, os, PIL, time
+from tensorflow.keras import layers
+
+import matplotlib.pyplot as plt
+
+from tqdm import trange
+
+(train_imgs, train_labs), (test_imgs, test_labs) = tf.keras.datasets.mnist.load_data()
+
+train_imgs = train_imgs.reshape(train_imgs.shape[0], 28, 28, 1).astype('float32')
+train_imgs = (train_imgs - 127.5) / 127.5 # Normalize the images to [-1, 1]
+
+BUFFER_SIZE = 60000
+BATCH_SIZE  = 256
+
+train_dataset = tf.data.Dataset.from_tensor_slices(train_imgs).shuffle(BUFFER_SIZE).batch(BATCH_SIZE)
+
+def make_generator_model():
+    model = tf.keras.Sequential()
+
+    model.add(layers.Dense(7*7*256, use_bias=False, input_shape=(100,)))
+    model.add(layers.BatchNormalization())
+    model.add(layers.LeakyReLU())
+
+    model.add(layers.Reshape((7, 7, 256)))
+
+    model.add(layers.Conv2DTranspose(128, (5, 5), strides=(1, 1), padding='same', use_bias=False))
+    model.add(layers.BatchNormalization())
+    model.add(layers.LeakyReLU())
+
+    model.add(layers.Conv2DTranspose(64, (5, 5), strides=(2, 2), padding='same', use_bias=False))
+    model.add(layers.BatchNormalization())
+    model.add(layers.LeakyReLU())
+
+    model.add(layers.Conv2DTranspose(1, (5, 5), strides=(2, 2), padding='same', use_bias=False))
+
+    return model
+
+def make_discriminator_model():
+    model = tf.keras.Sequential()
+
+    model.add(layers.Conv2D(64, (5, 5), strides=(2, 2), padding='same', input_shape=[28, 28, 1]))
+    model.add(layers.LeakyReLU())
+    model.add(layers.Dropout(0.3))
+
+    model.add(layers.Conv2D(128, (5, 5), strides=(2, 2), padding='same'))
+    model.add(layers.LeakyReLU())
+    model.add(layers.Dropout(0.3))
+
+    model.add(layers.Flatten())
+    model.add(layers.Dense(1))
+
+    return model
+
+generator     = make_generator_model()
+discriminator = make_discriminator_model()
+
+cross_entropy = tf.keras.losses.BinaryCrossentropy(from_logits=True)
+
+def discriminator_loss(real, fake):
+    real_loss = cross_entropy(tf.ones_like(real), real)
+    fake_loss = cross_entropy(tf.zeros_like(fake), fake)
+    return real_loss + fake_loss
+
+def generator_loss(fake):
+    return cross_entropy(tf.ones_like(fake), fake)
+
+gen_opt = tf.keras.optimizers.Adam(1e-4)
+disc_opt = tf.keras.optimizers.Adam(1e-4)
+
+checkpt_dir = './training_checkpoints'
+checkpt_pref = os.path.join(checkpt_dir, 'ckpt')
+checkpt = tf.train.Checkpoint(generator_optimizer=gen_opt,
+                              discriminator_optimizer=disc_opt,
+                              generator=generator, discriminator=discriminator)
+
+EPOCHS = 50
+noise_dim = 100
+num_examples_to_generate = 16
+
+seed = tf.random.normal([num_examples_to_generate, noise_dim])
+
+@tf.function
+def train_step(images):
+    noise = tf.random.normal([BATCH_SIZE, noise_dim])
+
+    with tf.GradientTape() as gen_tape, tf.GradientTape() as disc_tape:
+        generated_images = generator(noise, training=True)
+
+        real_out = discriminator(images, training=True)
+        fake_out = discriminator(generated_images, training=True)
+
+        gen_loss = generator_loss(fake_out)
+        disc_loss = discriminator_loss(real_out, fake_out)
+
+    grads_gen = gen_tape.gradient(gen_loss, generator.trainable_variables)
+    grads_disc = disc_tape.gradient(disc_loss, discriminator.trainable_variables)
+
+    gen_opt.apply_gradients(zip(grads_gen, generator.trainable_variables))
+    disc_opt.apply_gradients(zip(grads_disc, discriminator.trainable_variables))
+
+def gen_imgs(model, epoch, test):
+    predictions = model(test, training=False)
+
+    fig = plt.figure(figsize=(4, 4))
+
+    for i in range(predictions.shape[0]):
+        plt.subplot(4, 4, i+1)
+        plt.imshow(predictions[i, :, :, 0] * 127.5 + 127.5, cmap='gray')
+        plt.axiss('off')
+
+    plt.savefig('image_at_epoch_{:04d}.png'.format(epoch))
+    plt.show()
+
+def train(dataset, epochs):
+    for epoch in trange(epochs):
+        start = time.time()
+
+        for batch in dataset:
+            train_step(batch)
+
+        gen_imgs(generator, epoch, seed)
+        
+        if epoch % 10 == 0:
+            checkpt.save(file_prefix=checkpt_pref)
+
+    gen_imgs(generator, epoch, seed)
+
+train(train_dataset, EPOCHS)
+
+anim_file = 'dcgan.gif'
+
+with imageio.get_writer(anim_file, mode='I') as writer:
+  filenames = glob.glob('image*.png')
+  filenames = sorted(filenames)
+  last = -1
+  for i,filename in enumerate(filenames):
+    frame = 2*(i**0.5)
+    if round(frame) > round(last):
+      last = frame
+    else:
+      continue
+    image = imageio.imread(filename)
+    writer.append_data(image)
+  image = imageio.imread(filename)
+  writer.append_data(image)
