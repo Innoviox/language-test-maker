69e6a063b5fb7747ff814b531093ff3879d30c53
Simon Chervenak <sarras305@gmail.com>
2019-11-07 07:55:09 -0500

    i have hacked karl

--- karl.py
+++ karl.py
@@ -1,10 +1,24 @@
 import base
+import time
 
 base.driver.get("https://karl.qanta.org/")
 base.find("/html/body/div/div/div/main/div/div/div/div/div/div[1]/form/div[1]/div[2]/div[1]/div/input").send_keys("sarras305")
 base.find("/html/body/div/div/div/main/div/div/div/div/div/div[1]/form/div[2]/div[2]/div[1]/div/input").send_keys(open("karlpass.txt").read()+"\n")
 
-base.driver.get("https://karl.qanta.org/main/add/fact")
+time.sleep(3)
 
-print(word_map.keys())
-import pdb; pdb.set_trace()
+base.find("/html/body/div/div/div[3]/div[1]/aside/div[1]/div[3]/div[2]/a/div[2]").click()
+
+time.sleep(3)
+
+base.find("/html/body/div/div/div[3]/div[1]/main/div/div/div/div[2]/form/div[3]/div/div/div/div[1]/div[1]/div[1]").click()
+time.sleep(1)
+base.find("/html/body/div/div/div[1]/div/div/div[2]/a/div/div").click()
+
+for i in base.word_map.values():
+    for word, (trans, _, _) in i.items():
+        base.find("/html/body/div/div/div[3]/div[1]/main/div/div/div/div[2]/form/div[1]/div/div[1]/div/input").send_keys(word)
+        base.find("/html/body/div/div/div[3]/div[1]/main/div/div/div/div[2]/form/div[2]/div/div[1]/div/input").send_keys(trans)
+        base.find("/html/body/div/div/div[3]/div[1]/main/div/div/div/div[3]/button[3]").click()
+
+        time.sleep(1)        
